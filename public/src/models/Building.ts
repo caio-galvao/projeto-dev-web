9b8af9ae389fa7e483800e3d16dd500b832ce9cf
caio.galvao <caio.r.galvao@gmail.com>
2025-02-19 11:58:28 -0300

    feat: building model

---
+++ public/src/models/Building.ts
@@ -0,0 +1,50 @@
+import { Model, DataTypes, Optional, ForeignKey } from 'sequelize';
+import sequelize from '../config/database';
+import { Company } from './Company';
+
+interface BuildingAttributes {
+    id: number;
+    name: string;
+    company_id: string
+}
+
+interface BuildingCreationAttributes extends BuildingAttributes {}
+
+export class Building extends Model<BuildingAttributes, BuildingCreationAttributes> implements BuildingAttributes {
+    public id!: number;
+    public name!: string;
+    public company_id!: string;
+}
+// Inicialize o modelo com os campos no banco
+Building.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        company_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Company,
+                key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+        },
+        },
+        {
+        sequelize,
+        tableName: "buildings",
+        timestamps: false,
+    }
+);
+
+Building.belongsTo(Company, { foreignKey: 'company_id', as: 'manager' });
+Company.hasMany(Building, { foreignKey: 'company_id', as: 'companyBuildings' });
