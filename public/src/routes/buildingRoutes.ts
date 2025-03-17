6f500cdef1f21aa2e4c23bd7d6a5f2255e6c5d4e
caio.galvao <caio.r.galvao@gmail.com>
2025-03-16 21:35:14 -0300

    feat: abac authorization for building routes

--- public/src/routes/buildingRoutes.ts
+++ public/src/routes/buildingRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { authenticate, authorize } from '../middlewares/authMiddleware';
 import  BuildingController from "../controllers/buildingController";
+import { BuildingService } from "../services/buildingService";
+import { CompanyService } from "../services/companyService";
 
 export const buildingRoutes = Router();
+const buildingService = new BuildingService();
+const companyService = new CompanyService();
 
 
 buildingRoutes.post("/", authenticate, authorize(['master', 'ultra']), (req, res) => BuildingController.createBuilding(req, res));
@@ -11,6 +15,26 @@ buildingRoutes.get("/company/:company_id", authenticate, authorize(['admin', 'ma
 
 buildingRoutes.get("/:id", authenticate, (req, res) => BuildingController.getOneBuilding(req, res));
 
-buildingRoutes.put("/:id", authenticate, authorize(['master', 'ultra']), (req, res) => BuildingController.editOneBuilding(req, res));
+buildingRoutes.put("/:id", 
+    authenticate, 
+    authorize(['master', 'ultra'], async (req, user) => {
+        if (user.role != "ultra") {
+            const building = await buildingService.getOneBuilding(Number(req.params.id));
+            const company = await companyService.getOneCompany(Number(building?.company_id));
+            return company ? company.manager_id === user.id: false;
+        }
+        return true;
+    }),
+    (req, res) => BuildingController.editOneBuilding(req, res));
 
-buildingRoutes.delete("/:id", authenticate, authorize(['master', 'ultra']), (req, res) => BuildingController.deleteOneBuilding(req, res));
+buildingRoutes.delete("/:id", 
+    authenticate, 
+    authorize(['master', 'ultra'], async (req, user) => {
+        if (user.role != "ultra") {
+            const building = await buildingService.getOneBuilding(Number(req.params.id));
+            const company = await companyService.getOneCompany(Number(building?.company_id));
+            return company ? company.manager_id === user.id: false;
+        }
+        return true;
+    }),
+    (req, res) => BuildingController.deleteOneBuilding(req, res));
