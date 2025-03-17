0865e0733658eb3e79afeab6c5286e03cdba1e13
caio.galvao <caio.r.galvao@gmail.com>
2025-03-16 21:28:08 -0300

    feat: abac authorization for company routes

--- public/src/routes/companyRoutes.ts
+++ public/src/routes/companyRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authenticate, authorize } from '../middlewares/authMiddleware';
 import  CompanyController from "../controllers/companyController";
+import { CompanyService } from "../services/companyService";
 
 export const companyRoutes = Router();
+const companyService = new CompanyService();
 
 
 companyRoutes.get("/", authenticate, authorize(['ultra']), (req, res) => CompanyController.getAllCompanies(req, res));
@@ -11,6 +13,24 @@ companyRoutes.post("/", authenticate, authorize(['master', 'ultra']), (req, res)
 
 companyRoutes.get("/:id", authenticate, (req, res) => CompanyController.getOneCompany(req, res));
 
-companyRoutes.put("/:id", authenticate, authorize(['master', 'ultra']), (req, res) => CompanyController.editOneCompany(req, res));
+companyRoutes.put("/:id", 
+    authenticate, 
+    authorize(['master', 'ultra'], async (req, user) => {
+        if (user.role != "ultra") {
+            const company = await companyService.getOneCompany(Number(req.params.id));
+            return company ? company.manager_id === user.id: false;
+        }
+        return true;
+    }),
+    (req, res) => CompanyController.editOneCompany(req, res));
 
-companyRoutes.delete("/:id", authenticate, authorize(['master', 'ultra']), (req, res) => CompanyController.deleteOneCompany(req, res));
+companyRoutes.delete("/:id", 
+    authenticate, 
+    authorize(['master', 'ultra'], async (req, user) => {
+        if (user.role != "ultra") {
+            const company = await companyService.getOneCompany(Number(req.params.id));
+            return company ? company.manager_id === user.id: false;
+        }
+        return true;
+    }),
+    (req, res) => CompanyController.deleteOneCompany(req, res));
