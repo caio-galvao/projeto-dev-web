290c0eaa7515e238d5407f39fe75952862eda4b7
renanbcunderline <renan.bezerra@ccc.ufcg.edu.br>
2025-03-19 22:43:15 -0300

    feat: update test scripts and add user tests. BROKE

--- public/src/index.ts
+++ public/src/index.ts
@@ -9,25 +9,27 @@ import { workspaceRoutes } from "./routes/workspaceRoutes";
 import { reserveRoutes } from "./routes/reserveRoutes";
 import authRoutes from './routes/authRoutes';
 
-    console.log("🚀 Servidor rodando...");
+console.log("🚀 Servidor rodando...");
 
-    dotenv.config();
+dotenv.config();
 
-    const app = express();
-    app.use(express.json());
+const app = express();
+app.use(express.json());
 
-    app.use('/auth', authRoutes);
-    app.use("/users", userRoutes); 
-    app.use("/company", companyRoutes);
-    app.use("/building", buildingRoutes);
-    app.use("/room", roomRoutes);
-    app.use("/workspace", workspaceRoutes);
-    app.use("/reserve", reserveRoutes);
+app.use('/auth', authRoutes);
+app.use("/users", userRoutes); 
+app.use("/company", companyRoutes);
+app.use("/building", buildingRoutes);
+app.use("/room", roomRoutes);
+app.use("/workspace", workspaceRoutes);
+app.use("/reserve", reserveRoutes);
 
-    // Testando a conexão e inicializando o servidor
-    sequelize.sync({ force: true }).then(() => {
-        console.log("Banco de dados conectado!");
-        app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
-    }).catch((error) => {
-        console.error("Erro ao conectar ao banco de dados:", error);
-    });
+export default app;
+
+// Testando a conexão e inicializando o servidor
+sequelize.sync({ force: true }).then(() => {
+    console.log("Banco de dados conectado!");
+    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+}).catch((error) => {
+    console.error("Erro ao conectar ao banco de dados:", error);
+});
