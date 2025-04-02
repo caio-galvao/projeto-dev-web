a2316b7fba12283cfb8a551e5e5b75bc03920a72
renanbcunderline <renan.bezerra@ccc.ufcg.edu.br>
2025-04-01 22:22:27 -0300

    feat: add CORS support and update package dependencies

--- public/src/index.ts
+++ public/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import cors from "cors";
 import sequelize from "./config/database";
 import { userRoutes } from "./routes/userRoutes";
 import { companyRoutes } from "./routes/companyRoutes";
@@ -10,25 +11,31 @@ import { reserveRoutes } from "./routes/reserveRoutes";
 import { authenticate } from './middlewares/authMiddleware';
 import authRoutes from './routes/authRoutes';
 
-    console.log("🚀 Servidor rodando...");
+console.log("🚀 Servidor rodando...");
 
-    dotenv.config();
+dotenv.config();
 
-    const app = express();
-    app.use(express.json());
+const app = express();
+app.use(express.json());
 
-    app.use('/auth', authRoutes);
-    app.use("/users", userRoutes); 
-    app.use("/company", companyRoutes);
-    app.use("/building", buildingRoutes);
-    app.use("/room", roomRoutes);
-    app.use("/workspace", workspaceRoutes);
-    app.use("/reserve", reserveRoutes);  
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:8080',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
-    // Testando a conexão e inicializando o servidor
-    sequelize.sync({ force: true }).then(() => {
-        console.log("Banco de dados conectado!");
-        app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
-    }).catch((error) => {
-        console.error("Erro ao conectar ao banco de dados:", error);
-    });
+app.use('/auth', authRoutes);
+app.use("/users", userRoutes); 
+app.use("/company", companyRoutes);
+app.use("/building", buildingRoutes);
+app.use("/room", roomRoutes);
+app.use("/workspace", workspaceRoutes);
+app.use("/reserve", reserveRoutes);  
+
+// Testando a conexão e inicializando o servidor
+sequelize.sync({ force: true }).then(() => {
+    console.log("Banco de dados conectado!");
+    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+}).catch((error) => {
+    console.error("Erro ao conectar ao banco de dados:", error);
+});
